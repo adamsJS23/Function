8592374553bd58b215eabb897003d1981fa53109
adamsjs23 <adamaguebre@yahoo.fr>
2024-08-23 09:46:48 +0200

    Passing by reference vs passing by value

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-'use strict';
+"use strict";
+
+// DEFAULT PARAMETERS
+const bookings = [];
+
+function createBooking(
+  flightNum = "234",
+  numPassenger = 4,
+  price = numPassenger * 200
+) {
+  // ES 5
+  // flightNum ||= "234";
+  // numPassenger ||= 100;
+  // price ||= 1000;
+  const booking = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+  bookings.push(booking);
+  console.log(bookings);
+}
+
+createBooking();
+createBooking("A320FF", 200, 1000);
+createBooking(undefined, 30, 1000);
+
+// VALUES VS REFERENCE
+
+const flight = "NH320Y";
+const adam = {
+  name: "Adama",
+  passport: 20282323,
+};
+
+function checkIn(flightNum, passenger) {
+  flightNum = "LH340";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 20282323) {
+    alert("Check In");
+  } else alert("Wrong passport");
+}
+
+checkIn(flight, adam);
+console.log(flight);
+console.log(adam);
+
+function changedPassportNum(passenger) {
+  passenger.passport = Math.trunc(Math.random() * 100000);
+}
+
+changedPassportNum(adam);
+checkIn(flight,adam)
